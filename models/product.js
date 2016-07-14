ee3900a5380536d93421c9c6c778faaa6a0e0d90
Carlos Escalona <carlos.escalona89@gmail.com>
2016-07-14 11:31:54 -0400

    Add models and routes

---
+++ models/product.js
@@ -0,0 +1,14 @@
+
+// Required packages
+var restful = require('node-restful');
+var mongoose = restful.mongoose;
+
+// Schema
+var productSchema = new mongoose.Schema({
+    name: String,
+    desc:  String,
+    price: Number
+});
+
+// Return model
+module.exports = restful.model('Products', productSchema);
